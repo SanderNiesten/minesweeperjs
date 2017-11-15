4137208c8bf9c63ad38d63720168b7b0d1ab4314
SanderNiesten <sanderniesten@live.nl>
2017-11-15 17:41:44 +0100

    first commit and setup of hardcoded minesweeper blank and filled in

---
+++ minesweeper.js
@@ -0,0 +1,14 @@
+let blankLine = '  |   |  ';
+
+let blankLineNewLine = '  |   |  \n';
+let blankLineRepeat = blankLineNewLine.repeat(3);
+console.log('This is what an empty board would look like:')
+console.log(blankLineRepeat)
+
+guessLine = '1 |   |  ';
+bombLine = '  | B |  ';
+
+console.log('This is what a board with a guess and a bomb would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
