5a20c84ec48fb0c577ad5be4b6399db1eb10efdf
SanderNiesten <sanderniesten@live.nl>
2017-11-24 16:55:35 +0100

    join the elements of each row and the rows themselves together

--- minesweeper.js
+++ minesweeper.js
@@ -29,3 +29,9 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   // An important note: The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
   return board
 };
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
