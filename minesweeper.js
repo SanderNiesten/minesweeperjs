4e1112c9ad7ac154ebab96c42d5aa4ce9e55c45b
SanderNiesten <sanderniesten@live.nl>
2017-11-24 16:54:33 +0100

    create function to generate random bombs'
    klda
    end
    clear
    
    '

--- minesweeper.js
+++ minesweeper.js
@@ -9,3 +9,23 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
   return board
 };
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  };
+  // An important note: The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
+  return board
+};
