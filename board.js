0a8981bdb80493ba3d947d14564e5aa7a891bf70
SanderNiesten <sanderniesten@live.nl>
2017-12-17 16:54:22 +0100

    split into board and game files

--- board.js
+++ board.js
@@ -1,27 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex)
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Booooom! You blew up, the game is over!')
-      this._board.print()
-    } else if (this._board.hasSafeTiles()) {
-      console.log('Current Board: ');
-      this._board.print();
-    } else {
-      console.log('Congratulations, you won!');
-      this._board.print();
-    }
-  }
-}
-
-// add  Board.generatePlayerBoard() in class Board
-// number of tiles flipped. --; should this not be in the if else statement, to get no bugs when tile is already flipped
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberOfColumns;
